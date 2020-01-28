791a418d3afacd5fd6bde4bd457ffbd24e82a775
marcelobritonet <marcelobrito@marcelobrito.net>
2020-01-28 10:25:14 -0300

    feat: estructural

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
+	import Home from "./containers/Home.svelte";
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Home/>
 </main>
 
 <style>
---
+++ src/containers/Home.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class="wrapper">
+    <p>teste</p>
+</div>
+
+<style type="text/scss">
+    .wrapper {
+        p {
+            background: red;
+        }
+    }
+
+</style>
